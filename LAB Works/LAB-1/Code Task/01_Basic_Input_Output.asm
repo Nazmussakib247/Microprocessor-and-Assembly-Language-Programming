2aee581cc5503ec532ac83ed5cd714504dd9d599
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-30 00:09:23 +0600

    Basic input output

---
+++ LAB Works/LAB-1/Code Task/01_Basic_Input_Output.asm
@@ -0,0 +1,18 @@
+.MODEL
+.STACK 100H
+.DATA
+.CODE
+MAIN PROC
+    MOV AH,1 ;Taking input
+    INT 21H
+    MOV BL,AL
+    
+    MOV AH,2
+    MOV DL,BL 
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+    
+    MAIN ENDP
+END MAIN
