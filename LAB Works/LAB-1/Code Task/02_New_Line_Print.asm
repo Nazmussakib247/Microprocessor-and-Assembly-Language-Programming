86e6b037c43820409143e8da90e18a021034f894
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-30 00:10:22 +0600

    Adding new line printing

---
+++ LAB Works/LAB-1/Code Task/02_New_Line_Print.asm
@@ -0,0 +1,23 @@
+INCLUDE "EMU8086.INC"
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+
+MAIN PROC
+
+    MOV AH,1        ; INPUT
+    INT 21H
+    MOV BL,AL
+
+    PRINTN ""       ; NEW LINE 
+
+    MOV AH,2
+    MOV DL,BL       ; OUTPUT
+    INT 21H
+
+    MOV AH,4CH
+    INT 21H
+
+MAIN ENDP
+END MAIN
