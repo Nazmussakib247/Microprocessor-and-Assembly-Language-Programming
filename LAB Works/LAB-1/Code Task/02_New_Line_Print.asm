4cb405977c33170d3adfeb3f259ab53fca23217e
Nazmus Sakib <nazmuss247@gmail.com>
2025-05-01 00:01:58 +0600

    Corrected mismatch file

--- LAB Works/LAB-1/Code Task/02_New_Line_Print.asm
+++ LAB Works/LAB-1/Code Task/02_New_Line_Print.asm
@@ -1,23 +1,25 @@
-INCLUDE "EMU8086.INC"
 .MODEL SMALL
 .STACK 100H
 .DATA
 .CODE
 
 MAIN PROC
-
-    MOV AH,1        ; INPUT
+    MOV AH, 1        ; INPUT
     INT 21H
-    MOV BL,AL
+    MOV BL, AL
 
-    PRINTN ""       ; NEW LINE 
-
-    MOV AH,2
-    MOV DL,BL       ; OUTPUT
+    ; Print new line manually
+    MOV AH, 2
+    MOV DL, 0DH
     INT 21H
-
-    MOV AH,4CH
+    MOV DL, 0AH
     INT 21H
 
+    ; Print the input character
+    MOV DL, BL
+    INT 21H
+
+    MOV AH, 4CH
+    INT 21H
 MAIN ENDP
 END MAIN
