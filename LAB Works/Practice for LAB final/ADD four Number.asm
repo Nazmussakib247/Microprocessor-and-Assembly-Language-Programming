57463f04170d8454bade899dd4fb9f6da6575e19
Nazmus Sakib <nazmuss247@gmail.com>
2025-08-20 16:27:29 +0600

    add four number added

---
+++ LAB Works/Practice for LAB final/ADD four Number.asm
@@ -0,0 +1,21 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+mov ax, 10
+mov bx, 20
+add ax, bx
+mov cx, 30
+add ax, cx
+mov dx, 40
+add ax, dx
+int 3
+
+
+ret
+
+
+
+
